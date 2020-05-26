1e3663848e3d4edfd7d8f6130c831cc3ba96cff8
Hong Nguyen Nam <Hacker1945@Hacker1945.local>
2020-05-26 23:11:57 +0700

    REFACTOR: update post interface v1

---
+++ controller/database.php
@@ -0,0 +1,9 @@
+<?php
+	// config info database
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db_name = 'cho_eva';
+	// connect database
+	$connect = mysqli_connect($host, $user, $pass, $db_name) or die('Connect Wrong (･´з`･)');
+?>
